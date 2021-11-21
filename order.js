a057ce57a39204f53bae6ff4435480ca13df84c4
Facundo <facucanto123@gmail.com>
2021-11-21 15:52:02 -0300

    a

--- order.js
+++ order.js
@@ -1,8 +1,15 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-    const order = localStorage.getItem('order');
+    const order = localStorage.getItem("order");
+   
     if(order){
+        console.log("hola");
         const catalogOrder = JSON.parse(order);
+        const orderSelected = document.orderSelector(".order-selected");
+        orderSelected.querySelector(".title").innerText = catalogOrder.title;
+        orderSelected.querySelector(".price").innerText = catalogOrder.price;
+        const img = orderSelected.querySelector(".img");
+        img.setAttribute("src", `imagenes/${catalogOrder.id}.webp`);
     }
 
 }); 
