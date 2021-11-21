6067045b7667cbedf5484f29c8022220d1b3f88a
Facundo <facucanto123@gmail.com>
2021-11-21 13:31:14 -0300

    Modificaciones order.html y order.js

---
+++ order.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", () => {
+
+    const order = localStorage.getItem('order');
+    if(order){
+        const catalogOrder = JSON.parse(order);
+    }
+
+}); 
